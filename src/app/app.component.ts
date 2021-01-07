7c089fbda6f698b0384e904f099f3c44001a9a16
AlaaEldinAhmed <alaaeldin.muhammad@gmail.com>
2021-01-07 15:14:27 +0200

    Using DOCUMENT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,27 @@
-import { Component, OnInit } from "@angular/core";
-import { environment } from "../environments/environment";
+import { AfterViewInit, Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { environment } from '../environments/environment';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  title = "ng-checka11y";
-  ngOnInit(): void {
+export class AppComponent implements AfterViewInit {
+  constructor(@Inject(DOCUMENT) private doc: any) {}
+  ngAfterViewInit(): void {
     if (!environment.production) {
       this.checkA11yLinkInit();
     }
   }
   checkA11yLinkInit(): void {
-    const headElm = document.head;
-    const styleLink = document.createElement("link");
-    styleLink.rel = "stylesheet";
-    styleLink.href = "https://cdn.jsdelivr.net/npm/checka11y-css/checka11y.css";
+    const headElm: HTMLHeadElement = this.doc.head;
+    const styleLink: HTMLElement = this.doc.createElement('link');
+    styleLink.setAttribute('rel', 'stylesheet');
+    styleLink.setAttribute(
+      'href',
+      'https://cdn.jsdelivr.net/npm/checka11y-css/checka11y.css'
+    );
     headElm.appendChild(styleLink);
   }
 }
