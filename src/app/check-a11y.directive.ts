7301551cc6168d744d8d89aa95691c6ecbaba9c4
AlaaEldinAhmed <alaaeldin.muhammad@gmail.com>
2021-01-07 15:37:01 +0200

    Fixing linting issues

--- src/app/check-a11y.directive.ts
+++ src/app/check-a11y.directive.ts
@@ -1,9 +1,9 @@
-import { DOCUMENT } from "@angular/common";
-import { AfterViewInit, Directive, Inject } from "@angular/core";
-import { environment } from "src/environments/environment";
+import { DOCUMENT } from '@angular/common';
+import { AfterViewInit, Directive, Inject } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Directive({
-  selector: "[appCheckA11y]",
+  selector: '[appCheckA11y]',
 })
 export class CheckA11yDirective implements AfterViewInit {
   constructor(@Inject(DOCUMENT) private doc: Document) {}
@@ -16,11 +16,11 @@ export class CheckA11yDirective implements AfterViewInit {
 
   checkA11yLinkInit(): void {
     const headElm: HTMLHeadElement = this.doc.head;
-    const styleLink: HTMLElement = this.doc.createElement("link");
-    styleLink.setAttribute("rel", "stylesheet");
+    const styleLink: HTMLElement = this.doc.createElement('link');
+    styleLink.setAttribute('rel', 'stylesheet');
     styleLink.setAttribute(
-      "href",
-      "https://cdn.jsdelivr.net/npm/checka11y-css/checka11y.css"
+      'href',
+      'https://cdn.jsdelivr.net/npm/checka11y-css/checka11y.css'
     );
     headElm.appendChild(styleLink);
   }
