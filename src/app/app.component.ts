863fbc9878c759e418ba10fa17052e32b95ce33c
AlaaEldinAhmed <alaaeldin.muhammad@gmail.com>
2021-01-07 14:58:07 +0200

    Adding Checka11y link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { environment } from "../environments/environment";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "ng-checka11y";
+  ngOnInit(): void {
+    if (!environment.production) {
+      this.checkA11yLinkInit();
+    }
+  }
+  checkA11yLinkInit(): void {
+    const headElm = document.head;
+    const styleLink = document.createElement("link");
+    styleLink.rel = "stylesheet";
+    styleLink.href = "https://cdn.jsdelivr.net/npm/checka11y-css/checka11y.css";
+    headElm.appendChild(styleLink);
+  }
 }
